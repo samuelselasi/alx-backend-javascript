55bbb2f533713f0721dac241525bbeded4709158
samuelselasi <samelsekasi@gmail.com>
2023-09-28 17:39:57 +0000

    Implement class SkyHighBuilding that extends from Building with getter attributes for sqft and floors

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
