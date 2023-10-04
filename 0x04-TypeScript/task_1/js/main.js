60e11af01168aab12087fb1ff4b44817f453d4ac
samuelselasi <samelsekasi@gmail.com>
2023-10-04 14:02:38 +0000

    Add generated js file that logs Directors attributes

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -5,4 +5,12 @@ var teacher3 = {
     location: 'London',
     contract: false
 };
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17
+};
 console.log(teacher3);
+console.log(director1);
