2089f8c87b1dcb93dace5a3720152d015f6ebe42
samuelselasi <samelsekasi@gmail.com>
2023-10-04 13:37:05 +0000

    Add ts file that creates a Teacher interface with attributes

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
