42a959b2614523ceac9add18aa69c3a00e18b73b
samuelselasi <samelsekasi@gmail.com>
2023-11-20 12:49:49 +0000

    Add program that will be executed through command line

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+// Program that uses stdin and executes via command line
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
