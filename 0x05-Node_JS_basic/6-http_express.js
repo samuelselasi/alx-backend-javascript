9d7486e27ba15594b7bcbc7cefcbdccaaf98c4fc
samuelselasi <samelsekasi@gmail.com>
2023-11-20 21:48:53 +0000

    Add HTTP server with Express http module

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+// HTTP server with Express's http module
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
