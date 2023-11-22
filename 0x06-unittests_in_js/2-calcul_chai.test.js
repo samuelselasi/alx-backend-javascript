8f85b7a2c98c4330f421f5fc3a1841244cd7a3fc
samuelselasi <samelsekasi@gmail.com>
2023-11-22 21:25:58 +0000

    Add rewrite of the test suite, using expect from Chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,55 +6,43 @@ const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber type == SUM', () => {
   it('check the rounded part', () => {
-    chai.expect(calculateNumber('SUM', 1.4, 4.5), 6);
-    chai.expect(calculateNumber('SUM', 1, 3.7), 5);
-    chai.expect(calculateNumber('SUM', 1.2, 3.7), 5);
-    chai.expect(calculateNumber('SUM', 1.5, 3.7), 6);
+    chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    chai.expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+    chai.expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    chai.expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
   });
   it('check negative numbers', () => {
-    chai.expect(calculateNumber('SUM', -1, 3), 2);
-    chai.expect(calculateNumber('SUM', 1, -3.7), -3);
-    chai.expect(calculateNumber('SUM', -1.2, 3.7), 3);
-    chai.expect(calculateNumber('SUM', -1.5, -3.7), -5);
-  });
-  it('check valid args', () => {
-    chai.expect(Number.isNaN(calculateNumber('SUM', 1)), true);
-    chai.expect(Number.isNaN(calculateNumber('SUM')), true);
+    chai.expect(calculateNumber('SUM', -1, 3)).to.equal(2);
+    chai.expect(calculateNumber('SUM', 1, -3.7)).to.equal(-3);
+    chai.expect(calculateNumber('SUM', -1.2, 3.7)).to.equal(3);
+    chai.expect(calculateNumber('SUM', -1.5, -3.7)).to.equal(-5);
   });
 });
 describe('calculateNumber type == SUBTRACT', () => {
   it('check the rounded part', () => {
-    chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    chai.expect(calculateNumber('SUBTRACT', 1, 3.7), -3);
-    chai.expect(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
-    chai.expect(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+    chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    chai.expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+    chai.expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+    chai.expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
   });
   it('check negative numbers', () => {
-    chai.expect(calculateNumber('SUBTRACT', -1, 3), -4);
-    chai.expect(calculateNumber('SUBTRACT', 1, -3.7), 5);
-    chai.expect(calculateNumber('SUBTRACT', -1.2, 3.7), -5);
-    chai.expect(calculateNumber('SUBTRACT', -1.5, -3.7), 3);
-  });
-  it('check valid args', () => {
-    chai.expect(Number.isNaN(calculateNumber('SUBTRACT', 1)), true);
-    chai.expect(Number.isNaN(calculateNumber('SUBTRACT')), true);
+    chai.expect(calculateNumber('SUBTRACT', -1, 3)).to.equal(-4);
+    chai.expect(calculateNumber('SUBTRACT', 1, -3.7)).to.equal(5);
+    chai.expect(calculateNumber('SUBTRACT', -1.2, 3.7)).to.equal(-5);
+    chai.expect(calculateNumber('SUBTRACT', -1.5, -3.7)).to.equal(3);
   });
 });
 describe('calculateNumber type == DIVIDE', () => {
   it('check the rounded part', () => {
-    chai.expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    chai.expect(calculateNumber('DIVIDE', 0, 3.7), 0);
-    chai.expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    chai.expect(calculateNumber('DIVIDE', 4, 2), 2);
+    chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    chai.expect(calculateNumber('DIVIDE', 0, 3.7)).to.equal(0);
+    chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    chai.expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
   });
   it('check negative numbers', () => {
-    chai.expect(calculateNumber('DIVIDE', -1.4, 0), 'Error');
-    chai.expect(calculateNumber('DIVIDE', 0, -3.7), -0);
-    chai.expect(calculateNumber('DIVIDE', -1.4, 4.5), -0.2);
-    chai.expect(calculateNumber('DIVIDE', -4, -2), 2);
-  });
-  it('check valid args', () => {
-    chai.expect(Number.isNaN(calculateNumber('DIVIDE', 1)), true);
-    chai.expect(Number.isNaN(calculateNumber('DIVIDE')), true);
+    chai.expect(calculateNumber('DIVIDE', -1.4, 0)).to.equal('Error');
+    chai.expect(calculateNumber('DIVIDE', 0, -3.7)).to.equal(-0);
+    chai.expect(calculateNumber('DIVIDE', -1.4, 4.5)).to.equal(-0.2);
+    chai.expect(calculateNumber('DIVIDE', -4, -2)).to.equal(2);
   });
 });
