38afb8fa6bb6ce1101757de743fcdff67d379d46
samuelselasi <samelsekasi@gmail.com>
2023-11-22 20:00:57 +0000

    Add test for function that returns sum of 2 numbers

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,24 @@
 // tests for function calculateNumber that returns the sum of 2 numbers
-const calculateNumber = require('./0-calcul.js');
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
 const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('checks the output', () => {
+  it('check the rounded part', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('check negative numbers', () => {
+    assert.strictEqual(calculateNumber(-1, 3), 2);
+    assert.strictEqual(calculateNumber(1, -3.7), -3);
+    assert.strictEqual(calculateNumber(-1.2, 3.7), 3);
+    assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
+  });
+  it('check valid args', () => {
+    assert.strictEqual(Number.isNaN(calculateNumber(1)), true);
+    assert.strictEqual(Number.isNaN(calculateNumber()), true);
   });
 });
