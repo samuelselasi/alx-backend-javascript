914e2c2374bf91088f1248641d21c463e067eafc
samuelselasi <samelsekasi@gmail.com>
2023-11-23 15:02:50 +0000

    Add copy of files from 8-api

---
+++ 0x06-unittests_in_js/9-api/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-hooks': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ["5-payment.test.js"],
+      "rules": {
+        "jest/no-hooks": "off"
+      },
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
