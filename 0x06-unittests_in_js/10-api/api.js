eaa9ea3d674f7f1b65f4b46a6a8c113ed4e7500f
samuelselasi <samelsekasi@gmail.com>
2023-11-23 17:27:00 +0000

    Add enpoints to get available payments and login

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,4 +1,4 @@
-// module containing endpoint that returns welcome message on express server
+// module containing endpoints on express server
 const express = require('express');
 
 const app = express();
@@ -12,6 +12,19 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   const { id } = req.params;
 
   res.send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+  res.send(`Welcome ${username}`);
 });
 
 app.listen(port, () => {
